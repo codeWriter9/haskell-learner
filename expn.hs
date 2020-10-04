8ab31324f95b4a9820930570e65a0d018ebfb56f
codeWriter9 <nerdsubhankar2006@gmail.com>
2020-10-04 22:00:48 +0530

    Update expn.hs
    Add power x to the summation e function

--- expn.hs
+++ expn.hs
@@ -11,6 +11,13 @@ inverse :: (RealFloat  a) => a -> a
 inverse 1 = 1
 inverse a = 1 / a
 
+create_tuple :: Int -> [(Int, Int)]
+create_tuple a = [(a, b) | b <- [0..10]]
+
+func :: [(Int, Int)] -> Float
+func [] = 0.0
+func ((x, p):xs) = (fromIntegral(powern x p) / inverse (fromIntegral(factorial (fromIntegral x))))  + (func xs)
+
 expn :: [Int] -> Float
 expn [] = 0.0 
 expn (x:xs) = inverse (fromIntegral(factorial (fromIntegral x))) + (expn xs)
