72aa6b0e615acd823d6c510f10961a46f6593884
codeWriter9 <nerdsubhankar2006@gmail.com>
2020-10-04 21:22:45 +0530

    Add - function for power
    Add - function for power which will add e to the power x

--- expn.hs
+++ expn.hs
@@ -1,6 +1,11 @@
 factorial :: (Integral a) => a -> a  
 factorial 0 = 1  
-factorial n = n * factorial (n - 1)       
+factorial n = n * factorial (n - 1)
+
+powern :: Int -> Int -> Int  
+powern a 0 = 1 
+powern a 1 = a
+powern a b = a  * (powern a (b - 1))
 
 inverse :: (RealFloat  a) => a -> a
 inverse 1 = 1
